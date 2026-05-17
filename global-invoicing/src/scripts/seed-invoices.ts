5955d3ebbeba7eb287b00fd508bfd3040805008e
Jakub Gaj <jakubgaj@gmail.com>
2026-05-17 23:58:14 +0200

    feat: mock data for customers, invoices

--- global-invoicing/src/scripts/seed-invoices.ts
+++ global-invoicing/src/scripts/seed-invoices.ts
@@ -2,35 +2,29 @@ import { EventBridgeClient, PutEventsCommand, PutEventsRequestEntry } from "@aws
 import { configProps } from "../config/config";
 import { readFileSync } from "fs";
 
-const prefix: string = configProps.standard.prefix;
 const region: string = configProps.standard.region;
 const profile: string = configProps.standard.profile;
-
-const eventFiles = [
-  "src/events/invoice-de.event.json",
-  "src/events/invoice-dk.event.json",
-  "src/events/invoice-gb.event.json",
-  "src/events/invoice-pl.event.json",
-];
+const prefix: string = configProps.standard.prefix;
+const events = configProps.events.invoices;
+const mockData = "src/data/invoices.mock.json";
+const eventBus = `${prefix}-bus`;
 
 async function seedInvoices() {
   const entries: PutEventsRequestEntry[] = [];
 
-  for (const file of eventFiles) {
-    const content = JSON.parse(readFileSync(file, "utf-8"));
-    const events = Array.isArray(content) ? content : [content];
+  const content = JSON.parse(readFileSync(mockData, "utf-8"));
+  const invoices = Array.isArray(content) ? content : [content];
 
-    for (const event of events) {
-      entries.push({
-        EventBusName: `${prefix}-bus`,
-        Source: event.source,
-        DetailType: event.detailType,
-        Detail: JSON.stringify(event.detail),
-      });
-    }
+  for (const invoice of invoices) {
+    entries.push({
+      EventBusName: eventBus,
+      Source: events.source,
+      DetailType: events.detailType,
+      Detail: JSON.stringify(invoice),
+    });
   }
 
-  console.log(`Sending ${entries.length} events to ${prefix}-bus...`);
+  console.log(`Sending ${entries.length} events to ${eventBus}...`);
 
   const client = new EventBridgeClient({ profile: profile, region: region });
   const result = await client.send(new PutEventsCommand({ Entries: entries }));
@@ -38,7 +32,7 @@ async function seedInvoices() {
   if (result.FailedEntryCount && result.FailedEntryCount > 0) {
     console.error("Failed:", result.Entries);
   } else {
-    console.log(`Event sent to ${prefix}-bus`);
+    console.log(`Event sent to ${eventBus}`);
   }
   
 }
